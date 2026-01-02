4a68ae2ef74d0297049fa8416d67e03a379279d7
Adam Lui <adam@kudoai.com>
2026-01-01 19:43:30 -0800

    Split `bodyContent` into multi-lines for readability

--- src/templates/error.js
+++ src/templates/error.js
@@ -3,9 +3,13 @@ import * as header from './header.js'
 import * as footer from './footer.js'
 
 export function generate(certID, errMsg) {
-    const title = certID ? `Kudo COA #${certID} / Certificate Not Found` : 'System Error',
-          description = certID ? `Certificate # ${certID} not found` : 'System error occurred',
-          errContent = `<div class="error-message">Certificate # <strong>${certID}</strong> ${errMsg}.</div>`,
-          bodyContent = `${header.generate(certID)} ${errContent} ${footer.generate()}`
+    const title = certID ? `Kudo COA #${certID} / Certificate Not Found` : 'System Error'
+    const description = certID ? `Certificate # ${certID} not found` : 'System error occurred'
+    const errContent = `<div class="error-message">Certificate # <strong>${certID}</strong> ${errMsg}.</div>`
+    const bodyContent = `
+        ${header.generate(certID)}
+        ${errContent}
+        ${footer.generate()}
+    `
     return base.generate({ title, description, bodyContent })
 }
--- src/templates/home.js
+++ src/templates/home.js
@@ -4,8 +4,12 @@ import * as footer from './footer.js'
 import * as baseScript from '../scripts/base.js'
 
 export function generate() {
-    const title = 'Kudo Grading + Authentication',
-          description = 'Verify certificate authenticity with Kudo Grading & Authentication Services',
-          bodyContent = `${header.generate('')} ${footer.generate()} <script>${baseScript.generate()}</script>`
+    const title = 'Kudo Grading + Authentication'
+    const description = 'Verify certificate authenticity with Kudo Grading & Authentication Services'
+    const bodyContent = `
+        ${header.generate('')}
+        ${footer.generate()}
+        <script>${baseScript.generate()}</script>
+    `
     return base.generate({ title, description, bodyContent, bodyClass: 'homepage' })
 }
