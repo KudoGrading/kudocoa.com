c6cbfa6151e3ff155cf5ccc82256ae9948317134
Adam Lui <adam@kudoai.com>
2026-01-02 02:20:16 -0800

    Condensed `navArrowsHTML` init, renamed vars

--- src/components/nav-arrows.js
+++ src/components/nav-arrows.js
@@ -1,13 +1,16 @@
 export function generate(certID) {
-    const certNum = parseInt(certID),
-          prevCert = certNum > 1 ? String(certNum - 1).padStart(10, '0') : null,
-          nextCert = String(certNum + 1).padStart(10, '0')
-    let navArrowsHTML = '<div class="nav-arrows">'
-    if (prevCert)
-        navArrowsHTML += `<a href="https://kudocoa.com/${prevCert}" class="nav-arrow left" title="Previous Certificate">&lt;</a>`
-    else
-        navArrowsHTML += '<span class="nav-arrow left disabled" title="No Previous Certificate">&lt;</span>'
-    navArrowsHTML += `<a href="https://kudocoa.com/${nextCert}" class="nav-arrow right" title="Next Certificate">&lt;</a>`
-    navArrowsHTML += '</div>'
-    return { navArrowsHTML, prevCert, nextCert }
+    const certNum = parseInt(certID)
+    const prevCertNum = certNum > 1 ? String(certNum -1).padStart(10, '0') : null
+    const nextCertNum = String(certNum +1).padStart(10, '0')
+    const navArrowsHTML = `
+        <div class="nav-arrows">
+            ${ prevCertNum ?
+                `<a href="https://kudocoa.com/${prevCertNum}"
+                    class="nav-arrow left" title="Previous Certificate">&lt;</a>`
+              : '<span class="nav-arrow left disabled" title="No Previous Certificate">&lt;</span>'
+            }
+            <a href="https://kudocoa.com/${nextCertNum}" class="nav-arrow right" title="Next Certificate">&lt;</a>
+        </div>
+    `
+    return { navArrowsHTML, prevCertNum, nextCertNum }
 }
