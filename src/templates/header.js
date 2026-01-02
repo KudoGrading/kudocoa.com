c9a25668c2e77e3c0004eb395ad0370968c28890
Adam Lui <adam@kudoai.com>
2026-01-02 01:22:29 -0800

    Deleted trailing spaces

--- src/templates/header.js
+++ src/templates/header.js
@@ -4,7 +4,7 @@ export function generate(certID = '') {
             <div class="hero-big">KUDO</div>
             <div class="hero-small">GRADING + AUTHENTICATION</div>
         </div>
-        
+
         <div class="search-bar">
             <input type="number" id="certNumber" placeholder="Enter certificate number" autofocus value="${certID}">
             <button class="search-btn" id="verifyBtn">Verify Certificate</button>
