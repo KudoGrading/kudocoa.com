7508d0bec8a5e0d561b9286f8d86110bc5993946
Adam Lui <adam@kudoai.com>
2026-01-01 18:07:33 -0800

    Deleted line breaks, added comments

--- src/lib/string.js
+++ src/lib/string.js
@@ -1,11 +1,7 @@
-export function camelToTitleCase(str) {
-    return str
-        .replace(/([A-Z])/g, ' $1')
-        .replace(/^./, str => str.toUpperCase())
-        .replace(/\bAnd\b/gi, '/')
-}
+export function camelToTitleCase(str) { // e.g. 'gradedAndAuthenticatedBy' => 'Graded / Authenticated By'
+    return str.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()).replace(/\bAnd\b/gi, '/') }
 
-export function formatDate(dateStr) {
+export function formatDate(dateStr) { // e.g. '2026-1-1' => 'JANUARY 1, 2026'
     if (!dateStr) return 'NOT SPECIFIED'
     try {
         if (/^\d{4}-\d{1,2}$/.test(dateStr)) {
@@ -18,7 +14,7 @@ export function formatDate(dateStr) {
     } catch { return dateStr.toUpperCase() }
 }
 
-export function generatePageTitle(certID, certData) {
+export function generatePageTitle(certID, certData) { // e.g. 'Kudo COA #1 / NFL SuperPro #1 (1991) / Kudo Grading + Authentication'
     const yearMatch = (certData?.coverDate || certData?.publishDate)?.match(/\d{4}/),
           year = yearMatch ? ` (${yearMatch[0]})` : ''
     return `Kudo COA #${parseInt(certID)} / ${certData?.item || ''}${year} / Kudo Grading + Authentication`
