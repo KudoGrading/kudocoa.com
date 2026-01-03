84357fb63362fad08da9f4a06981b05f05b34db0
Adam Lui <adam@kudoai.com>
2026-01-03 13:52:14 -0800

    Eliminated elem IDs

--- src/server/components/img-embed.js
+++ src/server/components/img-embed.js
@@ -1,12 +1,11 @@
 export function generate({ imgURL, alt = '' }) {
-    const id = 'img_' + Date.now() + Math.random().toString(36).slice(2, 11)
     return `
         <div class="img-embed">
-            <span class="name-text" id="name_${id}">${alt}</span>
+            <span class="name-text">${alt}</span>
             <img src="${imgURL}" 
-                 alt="${alt}" class="dynamic-img" id="img_${id}"
-                 onload="document.getElementById('img_${id}').style.display='block'; document.getElementById('name_${id}').style.display='none'"
-                 onerror="this.style.display='none'" style="display:none">
+                 alt="${alt}" class="dynamic-img" style="display:none" onerror="this.style.display='none'"
+                 onload="this.style.display='block' ; this.previousElementSibling.style.display='none'"
+            >
         </div>
     `
 }
