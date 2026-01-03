f341dd9316dff61c3d9863cbae89b8d3f80bb852
Adam Lui <adam@kudoai.com>
2026-01-03 10:18:43 -0800

    Added options obj to `formatDate()`

--- src/server/lib/string.js
+++ src/server/lib/string.js
@@ -1,15 +1,17 @@
 export function camelToTitleCase(str) { // e.g. 'gradedAndAuthenticatedBy' => 'Graded / Authenticated By'
     return str.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()).replace(/\bAnd\b/gi, '/') }
 
-export function formatDate(dateStr) { // e.g. '2026-1-1' => 'JANUARY 1, 2026'
-    if (!dateStr) return 'NOT SPECIFIED'
+export function formatDate(dateStr, { format = 'monthDayYear', caseStyle = 'upper' } = {}) { // e.g. '2026-1' => 'JANUARY 2026'
+    if (!dateStr) throw new Error(`'dateStr' option required by formatDate()`)
     try {
-        if (/^\d{4}-\d{1,2}$/.test(dateStr)) {
-            const [year, month] = dateStr.split('-').map(Number),
-                  date = new Date(year, month - 1, 1)
-            return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long' }).toUpperCase()
-        }
-        const date = new Date(dateStr)
-        return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }).toUpperCase()
-    } catch (err) { return dateStr.toUpperCase() }
+        let result
+        if (/^\d{4}-\d{1,2}$/.test(dateStr) || format == 'monthYear') { // YYYY-MM input
+            const [year, month] = dateStr.split('-').map(Number)
+            result = new Date(year, month -1, 1).toLocaleDateString('en-US', { year: 'numeric', month: 'long' })
+        } else if (format == 'monthDayYear')
+            result = new Date(dateStr).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+        else
+            result = dateStr
+        return caseStyle == 'upper' ? result.toUpperCase() : result
+    } catch (err) { return dateStr }
 }
