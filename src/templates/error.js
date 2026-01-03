b46097b64428f8b9c9b837f3dcd6710549823c48
Adam Lui <adam@kudoai.com>
2026-01-02 18:28:02 -0800

    Moved `errorMsg` fallback to function sig, eliminated `errContent`, added sep to msg

--- src/templates/error.js
+++ src/templates/error.js
@@ -2,13 +2,14 @@ import * as base from './base.js'
 import * as header from './header.js'
 import * as footer from './footer.js'
 
-export function generate({ certID, errMsg } = {}) {
+export function generate({ certID, errMsg = 'Error' } = {}) {
     const title = certID ? `Kudo COA #${certID} / Certificate Not Found` : 'System Error'
     const description = certID ? `Certificate # ${certID} not found` : 'System error occurred'
-    const errContent = `<div class="error-msg">Certificate # <strong>${certID}</strong> ${ errMsg || 'Error' }.</div>`
     const bodyContent = `
         ${header.generate(certID)}
-        ${errContent}
+        <div class="error-msg">
+            ${ certID ? `Certificate # <strong>${certID}</strong> — ` : '' }${errMsg}
+        </div>
         ${footer.generate()}
     `
     return base.generate({ title, description, bodyContent })
