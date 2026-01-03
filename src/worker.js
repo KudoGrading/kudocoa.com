f4e729eae57b3a65662cd72e81c69d443d3f3407
Adam Lui <adam@kudoai.com>
2026-01-02 20:57:09 -0800

    Renamed `htmlHeader` to `htmlHeaders`

--- src/worker.js
+++ src/worker.js
@@ -5,22 +5,22 @@ import * as errPage from './templates/error.js'
 
 export default {
     async fetch(req, env) {
-        const url = new URL(req.url), htmlHeader = { 'Content-Type': 'text/html' }
+        const url = new URL(req.url), htmlHeaders = { 'Content-Type': 'text/html' }
 
         if (/^\/?$/.test(url.pathname)) // render homepage
-            return new Response(minify(homepage.generate()), { headers: htmlHeader })
+            return new Response(minify(homepage.generate()), { headers: htmlHeaders })
 
         // Validate cert #
         const certInput = url.pathname.split('/')[1]
         if (!/^\d+$/.test(certInput))
             return new Response(minify(errPage.generate({
                 certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)' })), {
-                    headers: htmlHeader, status: 400 }
+                    headers: htmlHeaders, status: 400 }
             )
         const certID = certInput.padStart(10, '0')
         if (certID.length > 10)
             return new Response(minify(errPage.generate({
-                certID, errMsg: 'Certificate ID too long (max 10 digits!)' })), { headers: htmlHeader, status: 400 })
+                certID, errMsg: 'Certificate ID too long (max 10 digits!)' })), { headers: htmlHeaders, status: 400 })
         if (certInput != certID) // redir e.g. /1 to /0000000001
             return Response.redirect('https://kudocoa.com/' + certID, 301)
 
@@ -29,12 +29,12 @@ export default {
             const certData = await env.COAS_KV.get(certID)
             return !certData ?
                 new Response(minify(errPage.generate({ certID, errMsg: 'Not found' })), {
-                    headers: htmlHeader, status: 404 })
+                    headers: htmlHeaders, status: 404 })
               : new Response(minify(await certPage.generate({ certID, certData })), {
-                    headers: { ...htmlHeader, 'Cache-Control': 'public, max-age=300' }})
+                    headers: { ...htmlHeaders, 'Cache-Control': 'public, max-age=300' }})
         } catch (err) {
             return new Response(minify(errPage.generate({ errMsg: 'System error' })), {
-                    headers: htmlHeader, status: 500 })
+                    headers: htmlHeaders, status: 500 })
         }
     }
 }
