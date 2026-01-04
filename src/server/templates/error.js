f9b745d6c3ffa65720c6d5e7e80f4c7509f6906b
Adam Lui <adam@kudoai.com>
2026-01-04 10:29:05 -0800

    Redesigned error pages to show big status code

--- src/server/templates/error.js
+++ src/server/templates/error.js
@@ -4,12 +4,15 @@ import * as footer from './footer.js'
 
 const app = await import('../../../data/app.json')
 
-export function generate({ certID, errMsg = 'Error' } = {}) {
+export function generate({ certID, errMsg = 'Error', status = 404 } = {}) {
     const title = `${ certID ? `Kudo COA #${certID} / Certificate Not Found` : 'System Error' } / ${app.name}`
     const description = certID ? `Certificate # ${certID} not found` : 'System error occurred'
     const bodyContent = `
         ${header.generate(certID)}
-        <div class="error-msg">${ certID ? `Certificate # <strong>${certID}</strong> — ` : '' }${errMsg}</div>
+        <div class="error-msg">
+            <div class="status-code">${status}</div>
+            <p>${ certID ? `Certificate # <strong>${certID}</strong> — ` : '' }${errMsg}</p>
+        </div>
         ${footer.generate()}
     `
     return base.generate({ title, description, bodyContent })
--- src/worker.js
+++ src/worker.js
@@ -33,13 +33,13 @@ export default {
             // Validate cert #
             const certInput = url.pathname.split('/')[1]
             if (/\D/.test(certInput))
-                return new Response(
-                    minify(errPage.generate({ certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)' })), {
+                return new Response(minify(errPage.generate({
+                    certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)', status: 400 })), {
                         headers: htmlHeaders, status: 400 })
             const certID = certInput.padStart(10, '0')
             if (certID.length > 10)
-                return new Response(
-                    minify(errPage.generate({ certID, errMsg: 'Certificate ID too long (max 10 digits!)' })), {
+                return new Response(minify(errPage.generate({
+                    certID, errMsg: 'Certificate ID too long (max 10 digits!)', status: 400 })), {
                         headers: htmlHeaders, status: 400 })
             if (certInput != certID && !isDevMode) // redir e.g. /1 to /0000000001
                 return Response.redirect(`${baseURL}/${certID}`, 301)
@@ -48,12 +48,12 @@ export default {
             try {
                 const certData = await env.COAS_KV.get(certID)
                 return !certData ?
-                    new Response(minify(errPage.generate({ certID, errMsg: 'Not found' })), {
+                    new Response(minify(errPage.generate({ certID, errMsg: 'Not found', status: 404 })), {
                         headers: htmlHeaders, status: 404 })
                   : new Response(minify(await certPage.generate({ certID, certData })), {
                         headers: { ...htmlHeaders, 'Cache-Control': 'public, max-age=300' }})
             } catch (err) {
-                return new Response(minify(errPage.generate({ errMsg: 'System error: ' + err.message })), {
+                return new Response(minify(errPage.generate({ errMsg: 'System error: ' + err.message, status: 500 })), {
                         headers: htmlHeaders, status: 500 })
             }
 
