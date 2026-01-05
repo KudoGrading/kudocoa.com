47ee179a5705283ceb094ef1a8b86a01c2d37ba2
Adam Lui <adam@kudoai.com>
2026-01-04 19:40:35 -0800

    Alphabetized functions

--- src/client/components/search.js
+++ src/client/components/search.js
@@ -1,13 +1,13 @@
+export function initSearch({ input, btn, baseURL }) {
+    btn.onclick = () => verify({ input, btn, baseURL })
+    input.addEventListener('keydown', ({ key }) => {
+        if (key == 'Enter') verify({ input, btn, baseURL }) })
+}
+
 export function verify({ input, btn, baseURL }) {
     const certNum = input.value.trim()
     if (!certNum) return alert('Please enter a certificate number')
     if (!/^\d+$/.test(certNum)) return alert('Certificate number must contain only digits')
     btn.disabled = true ; btn.textContent = 'Retrieving...'
     location.href = `${baseURL}/${certNum}`
-}
-
-export function initSearch({ input, btn, baseURL }) {
-    btn.onclick = () => verify({ input, btn, baseURL })
-    input.addEventListener('keydown', ({ key }) => {
-        if (key == 'Enter') verify({ input, btn, baseURL }) })
 }
