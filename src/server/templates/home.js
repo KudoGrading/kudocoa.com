cd3e0383f0ec0a37971d20e8273899d28ec09b29
Adam Lui <adam@kudoai.com>
2026-01-06 00:47:56 -0800

    Moved style injection to `base.generate()`

--- src/server/templates/home.js
+++ src/server/templates/home.js
@@ -1,16 +1,15 @@
 import * as base from './base.js'
 import * as header from './header.js'
 import * as footer from './footer.js'
+import css from '../styles/css/home.min.css'
 
 const app = await import('../../../public/data/app.json')
-import css from '../styles/css/home.min.css'
 
 export function generate(devMode) {
     app.urls.assetHost.app = devMode ? 'http://localhost:8888/assets' : app.urls.assetHost.app
     const title = app.names.medium
     const description = `Verify certificate authenticity with ${app.names.long}`
     const bodyContent = `
-        <style>${css}</style>
         ${header.generate()}
         ${footer.generate()}
         <script type="module">
@@ -18,5 +17,5 @@ export function generate(devMode) {
             initHomepage()
         </script>
     `
-    return base.generate({ title, description, bodyContent, bodyClass: 'homepage' })
+    return base.generate({ title, description, bodyContent, bodyClass: 'homepage', css })
 }
