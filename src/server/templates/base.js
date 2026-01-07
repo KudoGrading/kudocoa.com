37fb98ddc7207df365a9edda18f768b0069b7d8b
Adam Lui <adam@kudoai.com>
2026-01-06 18:16:36 -0800

    Indented HTML

--- src/server/templates/base.js
+++ src/server/templates/base.js
@@ -4,24 +4,24 @@ export function generate({ title = 'Untitled page', description = '', bodyConten
     return `
         <!DOCTYPE html>
         <html lang="en">
-        <head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-E45MKWKPC9"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date()); gtag('config', 'G-E45MKWKPC9');
-            </script>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>${title}</title>
-            <meta name="description" content="${description}">
-            <style>${globalCSS}</style>
-            ${ css ? `<style>${css}</style>` : '' }
-            <link rel="icon" type="image/x-icon" href="https://kudoai.com/assets/images/icons/kudoai/favicon.ico">
-        </head>
-        <body${ bodyClass ? ` class="${bodyClass}"` : '' }>
-            <div class="container">${bodyContent}</div>
-        </body>
+            <head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-E45MKWKPC9"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date()); gtag('config', 'G-E45MKWKPC9');
+                </script>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>${title}</title>
+                <meta name="description" content="${description}">
+                <style>${globalCSS}</style>
+                ${ css ? `<style>${css}</style>` : '' }
+                <link rel="icon" type="image/x-icon" href="https://kudoai.com/assets/images/icons/kudoai/favicon.ico">
+            </head>
+            <body${ bodyClass ? ` class="${bodyClass}"` : '' }>
+                <div class="container">${bodyContent}</div>
+            </body>
         </html>
     `
 }
