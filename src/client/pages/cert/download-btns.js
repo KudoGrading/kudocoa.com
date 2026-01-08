b97a9f66060b0e88689d8d4d79dfb6c829efb17f
Adam Lui <adam@kudoai.com>
2026-01-08 03:13:38 -0800

    Replaced `.getAttribute()` w/ `.dataset`

--- src/client/pages/cert/download-btns.js
+++ src/client/pages/cert/download-btns.js
@@ -2,8 +2,8 @@ export function initDownloadBtns() {
     document.querySelectorAll('.download-btn').forEach(btn =>
         btn.onclick = async event => {
             event.preventDefault()
-            const url = btn.getAttribute('data-url'),
-                  download = btn.getAttribute('data-filename')
+            const url = btn.dataset.url,
+                  download = btn.dataset.filename
             try {
                 const href = URL.createObjectURL(await (await fetch(url)).blob()),
                       a = Object.assign(document.createElement('a'), { href, download, style: 'display: none' })
