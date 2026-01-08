d4060d38554365762a5380a12929707e39928499
Adam Lui <adam@kudoai.com>
2026-01-08 04:12:22 -0800

    Added `location.origin` to initial log + modded links to final log

--- src/client/debug-mode.js
+++ src/client/debug-mode.js
@@ -3,13 +3,14 @@ import { createLogger } from '../shared/lib/log.js'
 export function initDebugMode() { // append ?debug to all app links
     if (!location.search.includes('debug')) return
     const log = createLogger({ prefix: 'initDebugMode()', debugMode: true })
-    log.debug('Initializing app links...')
-    let linksChangedCnt = 0
+    log.debug(`Initializing ${location.origin} links...`)
+    const moddedLinks = []
     document.querySelectorAll('a[href]').forEach(link => {
         if (link.href.includes(location.origin)) { // change app links
             const url = new URL(link.href)
-            if (!url.search) { url.search = '?debug' ; link.href = url.toString() ; linksChangedCnt++ }
+            if (!url.search) { url.search = '?debug' ; link.href = url.toString() ; moddedLinks.push(link.href) }
         }
     })
-    log.debug(`${ linksChangedCnt || 'No' } link${ linksChangedCnt == 1 ? '' : 's' } changed!`)
+    log.debug(`${ moddedLinks.length || 'No' } link${ moddedLinks.length == 1 ? '' : 's' } changed!`)
+    moddedLinks.forEach(link => log.debug(link))
 }
