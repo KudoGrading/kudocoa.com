198097e0b59177c92d75067e628a7b7ffce92855
Adam Lui <adam@kudoai.com>
2026-01-08 04:31:58 -0800

    Added debug logging to `initDownloadBtns()`

--- src/client/pages/cert/download-btns.js
+++ src/client/pages/cert/download-btns.js
@@ -1,14 +1,29 @@
+import { createLogger } from '../../../shared/lib/log.js'
+
 export function initDownloadBtns() {
-    document.querySelectorAll('.download-btn').forEach(btn =>
+    const log = createLogger({ prefix: 'initDownloadBtns()', debugMode: location.search.includes('debug') })
+    log.debug('Initializing download buttons...')
+    const downloadBtns = []
+    document.querySelectorAll('.download-btn').forEach(btn => {
         btn.onclick = async event => {
             event.preventDefault()
             const url = btn.dataset.url,
-                  download = btn.dataset.filename
+                  download = btn.dataset.filename,
+                  filetype = btn.textContent.trim()
+            log.debug(`${filetype} button clicked`)
             try {
                 const href = URL.createObjectURL(await (await fetch(url)).blob()),
                       a = Object.assign(document.createElement('a'), { href, download, style: 'display: none' })
                 document.body.append(a) ; a.click() ; a.remove() ; URL.revokeObjectURL(href)
-            } catch (err) { open(url, '_blank') }
+                log.debug(`${filetype} download initiated!`)
+            } catch (err) {
+                log.debug(`Failed to initiate ${btn} download!`)
+                open(url, '_blank')
+            }
         }
-    )
+        downloadBtns.push(btn)
+    })
+    const btnTypes = downloadBtns.map(btn => btn.textContent.trim())
+    log.debug(`${ downloadBtns.length || 'No' } button${ downloadBtns.length == 1 ? '' : 's' } initialized! (${
+        btnTypes.join(' + ')})`)
 }
