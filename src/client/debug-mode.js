ebe71c162e04efddb4f9698c93b1192c416931e1
Adam Lui <adam@kudoai.com>
2026-01-07 23:36:22 -0800

    Added debug logging

--- src/client/debug-mode.js
+++ src/client/debug-mode.js
@@ -1,8 +1,15 @@
+import { createLogger } from '../shared/lib/log.js'
+
 export function initDebugMode() { // append ?debug to all app links
     if (!location.search.includes('debug')) return
-    console.log('[DEBUG MODE]')
+    const log = createLogger({ prefix: 'initDebugMode()', debugMode: true })
+    log.debug('Initializing app links...')
+    let linksChangedCnt = 0
     document.querySelectorAll('a[href]').forEach(link => {
-        if (link.href.includes(location.origin) && !link.href.includes('?'))
-            link.href = link.href + '?debug'
+        if (link.href.includes(location.origin)) { // change app links
+            const url = new URL(link.href)
+            if (!url.search) { url.search = '?debug' ; link.href = url.toString() ; linksChangedCnt++ }
+        }
     })
+    log.debug(`${ linksChangedCnt || 'No' } link${ linksChangedCnt == 1 ? '' : 's' } changed!`)
 }
