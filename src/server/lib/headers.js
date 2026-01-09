d474d15ce5842fde32da7d87bd3dbdb7f5106e7a
Adam Lui <adam@kudoai.com>
2026-01-09 00:08:59 -0800

    Eliminated unneeded `config` init

--- src/server/lib/headers.js
+++ src/server/lib/headers.js
@@ -1,8 +1,7 @@
 export function create({ type, certData }) {
     if (type == 'html') return { 'Content-Type': 'text/html' }
     else if (type == 'cache') {
-        const { config } = globalThis,
-              hasVideo = /\\"(?:trailer|vide?o?|youtube|yt)URLs\\"/.test(JSON.stringify(certData))
+        const hasVideo = /\\"(?:trailer|vide?o?|youtube|yt)URLs\\"/.test(JSON.stringify(certData))
         return { 'Cache-Control':
             (config.cacheDuration == 'auto' && hasVideo || config.cacheDuration == 0) ?
                 'no-store, must-revalidate' : `public, max-age=${
