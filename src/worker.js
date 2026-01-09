bdcbd5523f94efd380616f5da6da76178b6a4ba4
Adam Lui <adam@kudoai.com>
2026-01-08 21:25:02 -0800

    Moved header vars into `headers`

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ const config = {
 export default {
     async fetch(req, env) {
         const url = new URL(req.url),
-              htmlHeaders = { 'Content-Type': 'text/html' },
+              headers = { html: { 'Content-Type': 'text/html' }},
               devMode = env.ENVIRONMENT == 'development',
               debugMode = url.searchParams.has('debug'),
               baseURL = devMode ? 'http://localhost:8888' : url.origin
@@ -25,7 +25,7 @@ export default {
 
         } else if (url.pathname == '/') { // render homepage
             const homepage = await import('./server/templates/home.js')
-            return new Response(await processHTML(homepage.generate(devMode)), { headers: htmlHeaders })
+            return new Response(await processHTML(homepage.generate(devMode)), { headers: headers.html })
         }
 
         // Validate cert #
@@ -34,12 +34,12 @@ export default {
         if (/\D/.test(certInput)) // 400 error
             return new Response(await processHTML(errPage.generate({
                 certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)', status: 400, devMode })), {
-                    headers: htmlHeaders, status: 400 })
+                    headers: headers.html, status: 400 })
         const certID = certInput.padStart(10, '0')
         if (certID.length > 10) // 400 error
             return new Response(await processHTML(errPage.generate({
                 certID, errMsg: 'Certificate ID too long (max 10 digits!)', status: 400, devMode })), {
-                    headers: htmlHeaders, status: 400 })
+                    headers: headers.html, status: 400 })
         if (certInput != certID) // 301 redir e.g. /1 to /0000000001
             return Response.redirect(`${baseURL}/${certID}${url.search}`, 301)
 
@@ -49,16 +49,16 @@ export default {
             if (!certData) // 404 error
                 return new Response(await processHTML(errPage.generate({
                     certID, errMsg: 'Not found', status: 404, devMode })), {
-                        headers: htmlHeaders, status: 404 })
+                        headers: headers.html, status: 404 })
             const certPage = await import('./server/templates/cert.js')
             const hasVideo = /\\"(?:trailer|vide?o?|youtube|yt)URLs\\"/.test(JSON.stringify(certData))
-            const cacheHeaders = { // shorter for video pages to allow rotation
+            headers.cache = { // shorter for video pages to allow rotation
                 'Cache-Control': hasVideo ? 'no-store, must-revalidate' : `public, max-age=${config.cacheDuration}` }
             return new Response(await processHTML(await certPage.generate({
-                certID, certData, devMode, debugMode })), { headers: { ...htmlHeaders, ...cacheHeaders }})
+                certID, certData, devMode, debugMode })), { headers: { ...headers.html, ...headers.cache }})
         } catch (err) { // 500 error
             return new Response(await processHTML(errPage.generate({
-                errMsg: `System error: ${err.message}`, status: 500, devMode })), { headers: htmlHeaders, status: 500 })
+                errMsg: `System error: ${err.message}`, status: 500, devMode })), { headers: headers.html, status: 500 })
         }
 
         async function processHTML(html) {
