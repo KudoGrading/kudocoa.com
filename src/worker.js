63b4a93950d9afacc937fe15e4f1f4acf5ae338b
Adam Lui <adam@kudoai.com>
2026-01-08 22:54:12 -0800

    Passed `devMode` 1st in `html.process()` calls for readability

--- src/worker.js
+++ src/worker.js
@@ -29,7 +29,7 @@ export default {
         } else if (url.pathname == '/') { // render homepage
             const homepage = await import('./server/templates/home.js')
             return new Response(
-                html.process({ html: homepage.generate(devMode), devMode }),
+                html.process({ devMode, html: homepage.generate(devMode) }),
                 { headers: headers.create({ type: 'html' })}
             )
         }
@@ -39,17 +39,15 @@ export default {
               errPage = await import('./server/templates/error.js')
         if (/\D/.test(certInput)) // 400 error
             return new Response(
-                html.process({ html: errPage.generate({
-                    certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)', status: 400, devMode
-                        })}, devMode),
+                html.process({ devMode, html: errPage.generate({
+                    certID: certInput, errMsg: 'Invalid certificate ID (numbers only!)', status: 400, devMode })}),
                 { headers: headers.create({ type: 'html' }), status: 400 }
             )
         const certID = certInput.padStart(10, '0')
         if (certID.length > 10) // 400 error
             return new Response(
-                html.process({ html: errPage.generate({
-                    certID, errMsg: 'Certificate ID too long (max 10 digits!)', status: 400, devMode
-                        })}, devMode),
+                html.process({ devMode, html: errPage.generate({
+                    certID, errMsg: 'Certificate ID too long (max 10 digits!)', status: 400, devMode })}),
                 { headers: headers.create({ type: 'html' }), status: 400 }
             )
         if (certInput != certID) // 301 redir e.g. /1 to /0000000001
@@ -60,20 +58,20 @@ export default {
             const certData = await env.COAS_KV.get(certID)
             if (!certData) // 404 error
                 return new Response(
-                    html.process({ html: errPage.generate({
-                        certID, errMsg: 'Not found', status: 404, devMode })}, devMode),
+                    html.process({ devMode, html: errPage.generate({
+                        certID, errMsg: 'Not found', status: 404, devMode })}),
                     { headers: headers.create({ type: 'html' }), status: 404 }
                 )
             const certPage = await import('./server/templates/cert.js')
             return new Response(
-                html.process({ html: await certPage.generate({
-                    certID, certData, devMode, debugMode })}, devMode),
+                html.process({ devMode, html: await certPage.generate({
+                    certID, certData, devMode, debugMode })}),
                 { headers: { ...headers.create({ type: 'html' }), ...headers.create({ type: 'cache', certData })}}
             )
         } catch (err) { // 500 error
             return new Response(
-                html.process({ html: errPage.generate({
-                    errMsg: `System error: ${err.message}`, status: 500, devMode })}, devMode),
+                html.process({ devMode, html: errPage.generate({
+                    errMsg: `System error: ${err.message}`, status: 500, devMode })}),
                 { headers: headers.create({ type: 'html' }), status: 500 }
             )
         }
