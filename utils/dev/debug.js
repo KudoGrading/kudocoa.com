2555d4179d532617082de6e8e25e55554dffa5d1
Adam Lui <adam@kudoai.com>
2026-01-08 16:36:43 -0800

    Abstracted `config.<ip|port>`

--- utils/dev/debug.js
+++ utils/dev/debug.js
@@ -5,8 +5,12 @@
 import { exec } from 'node:child_process'
 import open from 'open'
 
-const config = { noBuild: process.argv.some(arg => /--(?:no-?build|nb)/.test(arg)) },
-      colors = { bw: '\x1b[1;97m', bg: '\x1b[1;92m', nc: '\x1b[0m' }
+const config = {
+    ip: 'localhost',
+    port: 8888,
+    noBuild: process.argv.some(arg => /--(?:no-?build|nb)/.test(arg))
+}
+const colors = { bw: '\x1b[1;97m', bg: '\x1b[1;92m', nc: '\x1b[0m' }
 
 if (config.noBuild) startWrangler()
 else {
@@ -22,12 +26,12 @@ else {
 
 function startWrangler() {
     console.log(`${colors.bw}Starting dev server in ?debug mode${ config.noBuild ? ' (no build)' : '' }...${colors.nc}`)
-    const wrangler = exec('npx wrangler dev --remote --ip localhost --port 8888')
+    const wrangler = exec(`npx wrangler dev --remote --ip ${config.ip} --port ${config.port}`)
     wrangler.stdout.on('data', data => {
         const output = data.toString() ; process.stdout.write(output)
-        if (/Ready|http:\/\/localhost:8888/.test(output)) { // server ready
+        if (new RegExp(`Ready|http://${config.ip}:${config.port}`).test(output)) { // server ready
             console.log(`\n${colors.bg}✓ Server ready! Opening browser in ?debug mode...${colors.nc}`)
-            open('http://localhost:8888?debug')
+            open(`http://${config.ip}:${config.port}?debug`)
         }
     })
     wrangler.stderr.on('data', data => process.stderr.write(data.toString()))
