a7b665dc8d63b7995c4cb1816b5a013d511ab6f1
Adam Lui <adam@kudoai.com>
2026-01-09 09:24:08 -0800

    Moved comment

--- src/client/components/back-to-top.js
+++ src/client/components/back-to-top.js
@@ -14,7 +14,8 @@ export async function initBackToTop() {
         else bttBtn.classList.remove('visible')
     })
 
-    if (document.querySelector('.comic-pages')) // add back-to-top footer link
+    // Add back-to-top footer link
+    if (document.querySelector('.comic-pages'))
         document.querySelector('.footer-links')?.append(
             dom.create.elem('span', { className: 'footer-separator' }),
             dom.create.elem('a', { textContent: 'Back to Top ↑', onclick: scrollToTop })
