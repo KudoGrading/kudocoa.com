68e7a6dc4cf9ae1155468202a0eea8b12b03d368
Adam Lui <adam@kudoai.com>
2026-01-10 04:52:19 -0800

    Condensed `createLogger()` props

--- src/shared/lib/log.js
+++ src/shared/lib/log.js
@@ -1,10 +1,10 @@
 export function createLogger({ prefix = '', debugMode = false }) {
     return {
-        debug: (...args) => {
+        debug(...args) {
             if (!debugMode) return
             console.log(prefix ? `${prefix} >>` : '[DEBUG]', ...args)
         },
-        error: (...args) => {
+        error(...args) {
             console.error(prefix ? `${prefix} ERROR:` : '[ERROR]', ...args)
         }
     }
