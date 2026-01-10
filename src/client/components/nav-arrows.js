bbce01ff8e48e4a4243502f895e0f97c53b03aab
Adam Lui <adam@kudoai.com>
2026-01-10 04:45:17 -0800

    Added debug logging to `navArrows.createAppend()`

--- src/client/components/nav-arrows.js
+++ src/client/components/nav-arrows.js
@@ -1,8 +1,13 @@
+import { createLogger } from '../../shared/lib/log.js'
+
 export function createAppend() {
     const certID = /^\d{10}$/.exec(location.pathname.slice(1))?.[0] ; if (!certID) return
+    const log = createLogger({ prefix: 'navArrows.createAppend()', debugMode: location.search.includes('debug') })
+    log.debug('Initializing cert numbers...')
     const certNum = parseInt(certID)
     const prevCertNum = certNum > 1 ? String(certNum -1).padStart(10, '0') : null
     const nextCertNum = String(certNum +1).padStart(10, '0')
+    log.debug('Initializing HTML...')
     const navArrowsHTML = `
         <div class="nav-arrows">
             ${ prevCertNum ?
@@ -13,10 +18,13 @@ export function createAppend() {
             <a href="/${nextCertNum}${location.search}" class="nav-arrow right" title="Next Certificate">&lt;</a>
         </div>
     `
+    log.debug('Appending arrows...')
     document.body.insertAdjacentHTML('afterbegin', navArrowsHTML)
+    log.debug('Adding key listener...')
     document.addEventListener('keydown', ({ key }) => {
         if (['INPUT', 'TEXTAREA', 'SELECT'].includes(document.activeElement?.tagName)) return
         if (key == 'ArrowLeft' && prevCertNum) location.href = `/${prevCertNum}${location.search}`
         else if (key == 'ArrowRight') location.href = `/${nextCertNum}${location.search}`
     })
+    log.debug('Successfully completed!')
 }
