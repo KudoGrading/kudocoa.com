d5c7e55f9a7ff9c47cb7d98696b3b456982adccc
Adam Lui <adam@kudoai.com>
2026-01-21 17:13:52 -0800

    Deleted test log

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -1,6 +1,4 @@
 import * as search from '../components/search.js'
 
-console.log('home.js running')
-
 document.addEventListener('DOMContentLoaded', () =>
     search.init({ input: document.querySelector('.search-bar input'), autofocus: true }))
