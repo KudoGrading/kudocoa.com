595ad8621672cf5d263024a629ca8e120eb38355
Adam Lui <adam@kudoai.com>
2026-01-27 13:53:22 -0800

    Condensed comment

--- app.config.mjs
+++ app.config.mjs
@@ -1,5 +1,5 @@
 export default {
     env: { dev: { ip: 'localhost', port: 8888 }},
-    cacheDuration: 0, // int (secs) or 'auto' (6h if non-video page, 0s otherwise)
+    cacheDuration: 0, // int (secs) or 'auto' (0s if video page, 6h otherwise)
     minifyHTML: 'auto' // <true|false> or 'auto' (false if dev mode)
 }
